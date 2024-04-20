ffe97b864175a1a9dfd0ff33d72ad5729fb76325
K surya <ksuryaj1892@gmail.com>
2024-04-20 20:46:13 +0530

    DashUI

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function customerPage(){
+    return(
+        <div>
+            <h1>Customer Page</h1>
+        </div>
+    )
+
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function invoicePage(){
+    return(
+        <div>
+            <h1>Invoice Page</h1>
+        </div>
+    )
+}
